efa6016bf4e80715424299c42cb6f831fa54894a
Felipe Godinho <fegodinho@gmail.com>
2018-03-09 19:24:40 -0300

    Aula 33

---
+++ src/main/java/TesteCadastro.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TesteCadastro {
+	
+	@Test
+	public void testeTextField() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Felipe");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Godinho");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		new Select(driver.findElement(By.id("elementosForm:escolaridade"))).selectByVisibleText("Superior");
+		new Select(driver.findElement(By.id("elementosForm:esportes"))).selectByVisibleText("Natacao");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		
+		Assert.assertTrue(driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
+		Assert.assertTrue(driver.findElement(By.id("descNome")).getText().endsWith("Felipe"));
+		Assert.assertEquals("Sobrenome: Godinho", driver.findElement(By.id("descSobrenome")).getText());
+		Assert.assertEquals("Sexo: Masculino", driver.findElement(By.id("descSexo")).getText());
+		Assert.assertEquals("Comida: Pizza", driver.findElement(By.id("descComida")).getText());
+		Assert.assertEquals("Escolaridade: superior", driver.findElement(By.id("descEscolaridade")).getText());
+		Assert.assertEquals("Esportes: Natacao", driver.findElement(By.id("descEsportes")).getText());
+		
+		driver.quit();
+		
+		
+		
+	}
+
+}
