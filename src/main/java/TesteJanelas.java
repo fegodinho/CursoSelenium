65e2bcd6c485a2842587225d764752bd70f67cac
Felipe Godinho <fegodinho@gmail.com>
2018-03-10 18:57:26 -0300

    Aula 36 - WindowHandler

--- src/main/java/TesteJanelas.java
+++ src/main/java/TesteJanelas.java
@@ -20,5 +20,21 @@ public class TesteJanelas {
 		driver.quit();
 		
 	}
+	
+	@Test
+	public void deveInteragirComJanelasSemTitulo() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("buttonPopUpHard")).click();
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getWindowHandles());
+		driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+		driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+		driver.findElement(By.tagName("textarea")).sendKeys("E agora?");
+		
+		driver.quit();
+	}
 
 }
