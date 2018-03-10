78e49c6ed64c7e200b36241588c83b6680f857c2
Felipe Godinho <fegodinho@gmail.com>
2018-03-10 18:32:17 -0300

    Aula 35 - Janelas

---
+++ src/main/java/TesteJanelas.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteJanelas {
+	
+	@Test
+	public void deveInteragirComJanelas() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("buttonPopUpEasy")).click();
+		driver.switchTo().window("Popup");		
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+		driver.close();
+		driver.switchTo().window("");
+		driver.findElement(By.tagName("textarea")).sendKeys("E agora?");
+		
+		driver.quit();
+		
+	}
+
+}
