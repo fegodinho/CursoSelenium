4a5080973f5666bc5c783b5fd52a3cdb4cc15dd4
Felipe Godinho <Felipe Godinho@192.168.25.4>
2018-03-30 14:02:10 -0300

    Aula 55 - Interação dinamica com tabelas

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -116,5 +116,10 @@ public class TesteCampoTreinamento {
 		WebElement element = driver.findElement(By.id("elementosForm:nome"));
 		js.executeScript("arguments[0].style.border = arguments[1]", element,"solid 4px red");
 	}
+	
+	@Test
+	public void deveClicarBotaoTabela() {
+		dsl.clicarBotaoTabela("Escolaridade", "Mestrado", "Radio","elementosForm:tableUsuarios");
+	}
 			
 }
